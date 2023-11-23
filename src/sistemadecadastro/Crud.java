209a204027d33bed4a4f42a8d9293f6f23747936
Gabriel Lima <49318475+yuusha-sama@users.noreply.github.com>
2023-11-23 16:45:11 -0300

    mudei o nome
    nome foi de CrudCreate.java para Crud.java

--- src/sistemadecadastro/Crud.java
+++ src/sistemadecadastro/Crud.java
@@ -11,12 +11,12 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author Windows
  */
-public class CrudCreate extends javax.swing.JFrame {
+public class Crud extends javax.swing.JFrame {
 
     /**
      * Creates new form CrudCreate
      */
-    public CrudCreate() {
+    public Crud() {
         initComponents();
     }
 
@@ -291,14 +291,18 @@ public class CrudCreate extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CrudCreate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Crud.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CrudCreate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Crud.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CrudCreate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Crud.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CrudCreate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Crud.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -307,7 +311,7 @@ public class CrudCreate extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CrudCreate().setVisible(true);
+                new Crud().setVisible(true);
             }
         });
     }

8f60ea15c744ea314e8ff03a3ced934bb0e9226b
João Felipe Correia de Oliveira <101203356+JackHeroes@users.noreply.github.com>
2023-11-26 14:48:49 -0300

    melhoria na conexão do banco

--- src/sistemadecadastro/DatabaseManager.java
+++ src/sistemadecadastro/DatabaseManager.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 
 public class DatabaseManager {
     private static final String SUrl = "jdbc:MySQL://localhost:3306/uservault";
-    private static final String SUser = "root";
-    private static final String SPass = "Data@Fit";
+    private static final String SUser = "uservault";
+    private static final String SPass = "Uservault#2468";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(SUrl, SUser, SPass);
